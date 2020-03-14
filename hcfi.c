1ac3674c2e163bcc8693156c4aceab8f2b55ce29
mmoreno <mmoreno@nursoft.cl>
2020-03-14 16:17:58 -0300

    WIP: General schema in develop

--- hcfi.c
+++ hcfi.c
@@ -1,33 +1,61 @@
 // Imports
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Structs
 
-struct Entity{
-	int index,
-	int group,
-	float space,
-}
+struct Entity
+{
+	int index;
+	int group;
+	float space;
+};
 
-struct Room{
-	int index,
-	int floor,
-	float space,
-	int* adjacentRooms
-}
+struct Room
+{
+	int index;
+	int floor;
+	float space;
+	int *adjacentRooms;
+};
 
-struct Constraints{
-	int index,
-	int type,
-	int sorh,
-	int c1,
-	int c2
-}
+struct Constraints
+{
+	int index;
+	int type;
+	int c1;
+	int c2;
+};
 
 // Functions
 
+void constructInitialSolution(int *allocation, struct Constraints *softConstraints, struct Constraints *hardConstraints, struct Room *rooms, struct Entity *entities)
+{
+}
+
+void checkHard(int *allocation, struct Constraints *hardConstraints, struct Room *rooms, struct Entity *entities)
+{
+}
+
+void checkSoft(int *allocation, struct Constraints *softConstraints, struct Room *rooms, struct Entity *entities)
+{
+}
+
+int solutionValue(int *allocation, struct Constraints *softConstraints, struct Room *rooms, struct Entity *entities)
+{
+}
+
+void checkNeighborhood(int *allocation, int *visited)
+{
+}
+
 // Main
+
+int main(int argc, char **argv)
+{
+	printf("Hello World!\n");
+	return 0;
+}
